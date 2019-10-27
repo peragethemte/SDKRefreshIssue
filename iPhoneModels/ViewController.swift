2b948c75034778e75b325f7c198870ae70df1caa
Alexandre Giguere <agiguere@beyondtechnologies.ca>
2019-10-27 10:03:11 -0400

    Initial Commit

---
+++ iPhoneModels/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iPhoneModels
+//
+//  Created by Alexandre Giguere on 2019-10-27.
+//  Copyright © 2019 Alexandre Giguere. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
